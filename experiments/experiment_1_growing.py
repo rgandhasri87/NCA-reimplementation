15bc37b8cc3a4fdac57c469569616205fc6fcc25
Rutvik Gandhasri <rgandhasri87@gmail.com>
2026-03-01 16:11:33 -0800

    Add starter growing experiment notebook ; right now just verifies the image loading code

---
+++ experiments/experiment_1_growing.py
@@ -0,0 +1,36 @@
+import marimo
+
+__generated_with = "0.20.2"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    from nca_utils import load_emoji
+
+    emoji_input = mo.ui.text(
+        value="🫏",
+        label="Enter emoji to use as target",
+        placeholder="🫏"
+    )
+
+    emoji_input
+    return emoji_input, load_emoji
+
+
+@app.cell
+def _(emoji_input, load_emoji):
+    target_np = load_emoji(emoji_input.value)
+    target_np
+    return
+
+
+if __name__ == "__main__":
+    app.run()
